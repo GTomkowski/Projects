214a521fc465cc161cd761f28d383bf5b14b2860
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-19 22:26:12 +0200

    edit add function to additionally remove input text upon clicking event

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const addBtn = document.querySelector('.todo-btn-style')
-
 const input = document.querySelector('input').value;
+
+/* define a function that will distinguish whether .list-btn-style has
+innerHTML = edit or innerhtml = X and will use different callbacks */
+
+
+
+
 
 let listStore = null;
 function add() {
@@ -12,18 +18,19 @@ function add() {
     const button1 = document.createElement('button')
     const button2 = document.createElement('button')
     paragraph.innerHTML = inputVal;
+    document.querySelector('input').value = null;
     button1.classList.add('list-btn-style')
-    button1.textContent = 'OK'
+    button1.textContent = 'EDIT'
     button2.classList.add('list-btn-style')
     button2.textContent = 'X'
 
 
     list.appendChild(listElement)
     listElement.append(paragraph,button1,button2)
-    console.log(list);
 
     
 }
+
 
 
 
