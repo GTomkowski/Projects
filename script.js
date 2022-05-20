2bf474f8d8c3712844bceb2b466296b374613e63
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-20 17:29:57 +0200

    defined a function remove

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 const addBtn = document.querySelector('.todo-btn-style')
-const input = document.querySelector('input').value;
+const addBtns = document.querySelectorAll('button')
+console.log(addBtn)
+console.log(addBtns);
+
+
 
 /* define a function that will distinguish whether .list-btn-style has
 innerHTML = edit or innerhtml = X and will use different callbacks */
 
+/* so what else do i need? two callbacks, one for deleting the list item and the second one for editing it */
 
+/* adding also enter key to add a task to the list */
 
-
-
-let listStore = null;
+const list = document.querySelector('ul')
+const firstElement = document.getElementById('first')
 function add() {
-    const inputVal = document.querySelector('input').value;
-    const list = document.querySelector('ul')
-    const listElement = document.createElement('li')
-    listElement.classList.add('d-f')
-    const paragraph = document.createElement('p')
-    const button1 = document.createElement('button')
-    const button2 = document.createElement('button')
-    paragraph.innerHTML = inputVal;
-    document.querySelector('input').value = null;
-    button1.classList.add('list-btn-style')
-    button1.textContent = 'EDIT'
-    button2.classList.add('list-btn-style')
-    button2.textContent = 'X'
+	const inputVal = document.querySelector('input').value
+	const paragraph = document.createElement('p')
+	const listElement = document.createElement('li')
+	listElement.classList.add('d-f')
+	paragraph.innerHTML = inputVal
+	document.querySelector('input').value = null
+	const button1 = document.createElement('button')
+	const button2 = document.createElement('button')
+	button1.classList.add('list-btn-style')
+	button1.textContent = 'X'
+	button2.classList.add('list-btn-style')
+	button2.textContent = 'EDIT'
 
-
-    list.appendChild(listElement)
-    listElement.append(paragraph,button1,button2)
-
-    
+	list.appendChild(listElement)
+	listElement.append(paragraph, button1, button2)
+	return listElement
 }
 
+function remove() {
+	listElement.remove()
+	if (firstElement) {
+		firstElement.remove()
+	}
+}
 
+/* co potrzebuje zrobic jeszcze
 
+jezeli dodany element istnieje to go usun */
 
 addBtn.addEventListener('click',add)
