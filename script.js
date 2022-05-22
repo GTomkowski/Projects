2ba9f507dd552dd1eb90a9186c0121c47936e5d2
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-22 17:56:57 +0200

    redefined remove function

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
 const addBtn = document.querySelector('.todo-btn-style')
-const addBtns = document.querySelectorAll('button')
-console.log(addBtn)
-console.log(addBtns);
-
-
-
-/* define a function that will distinguish whether .list-btn-style has
-innerHTML = edit or innerhtml = X and will use different callbacks */
-
-/* so what else do i need? two callbacks, one for deleting the list item and the second one for editing it */
-
-/* adding also enter key to add a task to the list */
 
 const list = document.querySelector('ul')
-const firstElement = document.getElementById('first')
+const storage = []
 function add() {
-	const inputVal = document.querySelector('input').value
-	const paragraph = document.createElement('p')
-	const listElement = document.createElement('li')
-	listElement.classList.add('d-f')
-	paragraph.innerHTML = inputVal
-	document.querySelector('input').value = null
-	const button1 = document.createElement('button')
-	const button2 = document.createElement('button')
-	button1.classList.add('list-btn-style')
+	const inputVal = document.querySelector('input').value /* pobieram wartosc z inputa do zmiennej inputVal */
+	const listElement = document.createElement('li') /* tworze element li */
+	const paragraph = document.createElement('p') /* tworze paragraf */
+	listElement.classList.add('d-f') /* dodaje klase d-f do paragrafu */
+	paragraph.innerHTML = inputVal /* dopisuje tresc ze zmiennej inputval do html paragrafu */
+	document.querySelector('input').value = null /* czyszcze wartosc inputa */
+	const button1 = document.createElement('button') /* tworze przycisk i przypisuje go do zmiennej button1*/
+	const button2 = document.createElement('button') /* tworze przycisk i przypisuje go do zmiennej button2*/
+	button1.classList.add('list-btn-style') 
 	button1.textContent = 'X'
 	button2.classList.add('list-btn-style')
 	button2.textContent = 'EDIT'
 
 	list.appendChild(listElement)
 	listElement.append(paragraph, button1, button2)
-	return listElement
+	storage.push(listElement)
+	return storage;
 }
 
 function remove() {
-	listElement.remove()
-	if (firstElement) {
-		firstElement.remove()
-	}
+	const listElement = document.querySelector('li')
+	list.removeChild(listElement)
 }
 
-/* co potrzebuje zrobic jeszcze
 
-jezeli dodany element istnieje to go usun */
-
-addBtn.addEventListener('click',add)
+addBtn.addEventListener('click', add)
